9afaaec285f2ec376c8d6a0565ea1cc20fc55698
Ritik <sadhritik6@gmail.com>
2026-08-21 19:15:27 +0530

    Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -13,22 +13,26 @@ public class BubbleSort {
             arr[i] = sc.nextInt();
         }
 
-        for(int i=1;i<n-1;i++){
-            for(int j=0;j<=n-2;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-
         }
-         
         for(int i=0;i<n;i++){
              System.out.print(arr[i] +" ");
         }
+
+        }
+         
+        
         
 
         
     }
-}
