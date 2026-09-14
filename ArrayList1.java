caec75d47deae1ed979bdf2d9245da65e5932b3b
Ritik <sadhritik6@gmail.com>
2026-09-14 19:19:36 +0530

    Arraylist

--- ArrayList1.java
+++ ArrayList1.java
@@ -6,9 +6,20 @@ public class ArrayList1 {
         
         ArrayList <Integer> li=new ArrayList<>();
         li.add(10);
+        li.add(20);
+        li.add(1,19);
+
+         ArrayList <Integer> li2=new ArrayList<>();
+         li2.add(25);
+         li2.addAll(li);
+
+        for(int i=0;i<li.size();i++){
+           System.out.print(li.get(i)+ " ");
+        }
 
         
 
         System.out.println(li);
+         System.out.println(li2);
     }
 }
