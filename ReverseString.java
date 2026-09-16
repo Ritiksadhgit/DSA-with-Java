d80da394472d8e0975a9be2cecf869142a51fe26
Ritik <sadhritik6@gmail.com>
2026-09-16 13:19:46 +0530

    Reverse String

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        StringBuilder sb=new StringBuilder( new String(s));
+        sb.reverse();
+        for(int i=0;i<sb.length();i++){
+            s[i]=sb.charAt(i);
+        }
+       
+    }
+    
+} 
+    
+
